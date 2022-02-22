4820ef8d8388067f19234b46b83055962f3a08df
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2022-02-22 14:53:03 +0700

    Update Chapter4 Ex1

---
+++ src/UDPSocket/Ex1.java
@@ -0,0 +1,25 @@
+package UDPSocket;
+
+import java.net.*;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        String s = "This is a test.";
+        byte[] data = s.getBytes();
+        try {
+            InetAddress ia = InetAddress.getByName("www.google.com");
+            int port = 7;
+            DatagramPacket dp
+                    = new DatagramPacket(data, data.length, ia, port);
+            System.out.println("This packet is addressed to "
+                    + dp.getAddress() + " on port " + dp.getPort());
+            System.out.println("There are " + dp.getLength()
+                    + " bytes of data in the packet");
+            System.out.println(
+                    new String(dp.getData(), dp.getOffset(), dp.getLength()));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
