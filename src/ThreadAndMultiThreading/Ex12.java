0e6c0a494ea678cef8f7e0bbc6a20a274b2614af
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2022-02-22 18:26:08 +0700

    Update Ex12

---
+++ src/ThreadAndMultiThreading/Ex12.java
@@ -0,0 +1,90 @@
+package ThreadAndMultiThreading;
+
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+import java.text.DecimalFormat;
+
+public class Ex12 extends JLabel {
+    private volatile String timeText;
+
+    private final Thread internalThread;
+
+    private volatile boolean noStopRequested;
+
+    public Ex12() {
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        setHorizontalAlignment(SwingConstants.RIGHT);
+        setFont(new Font("SansSerif", Font.BOLD, 16));
+        setText("00000.0"); // use to size component
+        setMinimumSize(getPreferredSize());
+        setPreferredSize(getPreferredSize());
+        setSize(getPreferredSize());
+
+        timeText = "0.0";
+        setText(timeText);
+
+        noStopRequested = true;
+        Runnable r = () -> {
+            try {
+                runWork();
+            } catch (Exception x) {
+                x.printStackTrace();
+            }
+        };
+
+        internalThread = new Thread(r, "DigitalTimer");
+        internalThread.start();
+    }
+
+    private void runWork() {
+        long startTime = System.currentTimeMillis();
+        int tenths = 0;
+        long normalSleepTime = 100;
+        long nextSleepTime = 100;
+        DecimalFormat fmt = new DecimalFormat("0.0");
+
+        Runnable updateText = () -> setText(timeText);
+
+        while (noStopRequested) {
+            try {
+                Thread.sleep(nextSleepTime);
+
+                tenths++;
+                long currTime = System.currentTimeMillis();
+                long elapsedTime = currTime - startTime;
+
+                nextSleepTime = normalSleepTime + ((tenths * 100L) - elapsedTime);
+
+                if (nextSleepTime < 0) {
+                    nextSleepTime = 0;
+                }
+
+                timeText = fmt.format(elapsedTime / 1000.0);
+                SwingUtilities.invokeAndWait(updateText);
+            } catch (InterruptedException ix) {
+                return;
+            } catch (InvocationTargetException x) {
+                x.printStackTrace();
+            }
+        }
+    }
+
+    public void stopRequest() {
+        noStopRequested = false;
+        internalThread.interrupt();
+    }
+
+    public boolean isAlive() {
+        return internalThread.isAlive();
+    }
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.getContentPane().setLayout(new FlowLayout());
+        f.getContentPane().add(new Ex12());
+        f.setSize(250, 100);
+        f.setVisible(true);
+    }
+
+}
