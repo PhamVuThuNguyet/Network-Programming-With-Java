262de91487fcf9ac2093c028b0d0b8300eb25d6d
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2022-02-22 15:17:27 +0700

    Update Ex8

---
+++ src/UDPSocket/Ex8.java
@@ -0,0 +1,36 @@
+
+package UDPSocket;
+
+import java.net.*;
+
+public class Ex8 extends Ex6 {
+
+    public final static int DEFAULT_PORT = 2002;
+
+    public Ex8() throws SocketException {
+        super(DEFAULT_PORT);
+    }
+
+    public void respond(DatagramPacket packet) {
+
+        byte[] data = new byte[packet.getLength()];
+        System.arraycopy(packet.getData(), 0, data, 0, packet.getLength());
+        try {
+            String s = new String(data, "8859_1");
+            System.out.println(packet.getAddress() + " at port "
+                    + packet.getPort() + " says " + s);
+        } catch (java.io.UnsupportedEncodingException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            Ex6 server = new Ex8();
+            server.start();
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
